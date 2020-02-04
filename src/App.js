345fc26618026b6028d83370ffd96e85d37ea71b
Danilo Barion <danilo.barion@vagas.com.br>
2020-02-04 15:32:21 -0300

    Add react polling test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactPolling from 'react-polling';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <ReactPolling
+        url={'http://api-candidatos-qa.ad.vagastec.com.br/api/v1/healthcheck'}
+        interval= {3000}
+        retryCount={3}
+        onSuccess={() => {
+            console.log('handle success');
+            return true
+          }
+        }
+        onFailure={() => console.log('handle success')}
+        method={'GET'}
+        body={ { 'teste': true } }
+        render={({ startPolling, stopPolling, isPolling }) => {
+          if (isPolling) {
+            return (
+              <div> Hello I am polling</div>
+            );
+          } else {
+            return (
+              <div> Hello I stopped polling</div>
+            );
+          }
+        }}
+      />
     </div>
   );
 }
