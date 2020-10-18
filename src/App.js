3001f52f615d4cae780461c88967b3d0bf367d90
le-santos <lequinterio@gmail.com>
2020-10-17 21:24:52 -0300

    imports new components to render method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import ReactPolling from 'react-polling'; 
+import DailyForecastCard from './Components/DailyForecastCard';
+import ForecastTitle from './Components/ForecastTitle';
+import HeatIndicator from './Components/HeatIndicator';
+import WeatherCondition from './Components/WeatherCondition';
+import WeatherHumidity from './Components/WeatherHumidity';
+import WeatherTemp from './Components/WeatherTemp';
+import WeatherDate from "./Components/WeatherDate";
 
 function App() {
   const apiKey = process.env.REACT_APP_WEATHER_API_KEY
@@ -12,10 +19,27 @@ function App() {
     let date = new Date(unixTimestamp * 1000);
     let day = date.getDate(); 
     let month = date.getMonth() + 1;
-    let year = date.getFullYear();
+    /* let year = date.getFullYear(); */
 
-    return `${day}/${month}/${year}` 
+    return `${day}/${month}` 
   }
+
+  const CardWeatherData = weatherData.map((day, index) => {
+    return (
+      <React.Fragment>
+        <DailyForecastCard key={index}>
+          <WeatherDate date={convertTimestampToDate(day.dt)}/>
+          <WeatherCondition />
+          <HeatIndicator />
+          <WeatherTemp 
+            maxTemp={Math.round(day.temp.max) + "º"}
+            minTemp={Math.round(day.temp.min) + "º"}
+          />
+          <WeatherHumidity humidity={day.weather.main}/>
+        </DailyForecastCard>
+      </React.Fragment>
+    )
+    })
 
   return (
     <div className="App">
@@ -37,12 +61,8 @@ function App() {
           if (isPolling) {
             return (
               <div> 
-              <h2>This is the seven days weather forecast</h2>
-              {weatherData.map((day, index) => {
-                return (
-                  <p key={index}>{convertTimestampToDate(day.dt)} => Min Temp: {day.temp.min} ºC, Max Temp: {day.temp.max} ºC</p>
-                )
-                })}
+              <ForecastTitle title={"5-day weather forecast for Sao Paulo"}/>
+              {CardWeatherData}
               </div>
             );
           } else {
@@ -57,3 +77,13 @@ function App() {
 }
 
 export default App;
+
+/* 
+
+To DO:
+
+- create logic for display heat color box
+- remove dec
+
+
+*/
